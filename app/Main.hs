9a42dac02585f47c6de5bf5afa102688df88d427
Tazmilur Saad <ssaad@colgate.edu>
2023-11-16 23:02:08 -0500

    Testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+  import Network.Wreq hiding (statusCode)
+  import Control.Lens
+  import Network.HTTP.Types
+  import Data.Text (Text)
+  
+  getReleases :: IO ()
+  getReleases = 
+    let 
+      githubApiVersion = "2022-11-28"
+      kokaReleasePrefix = "https://api.github.com/repos/koka-lang/koka/releases"
+      opts = defaults & header "Accept" .~ ["application/vnd.github+json"] & header "X-GitHub-Api-Version" .~ [githubApiVersion]
+    in do 
+      r <- getWith opts kokaReleasePrefix
+      case r ^. responseStatus of
+        Status { statusCode = 200 } -> print success
+        Status _ _ -> print failure
+      where 
+        success :: Text = "Success :)"
+        failure :: Text = "Failure :("
+
+      -- print $ r ^. responseBody . key "Assets" . _String
+
+  main :: IO ()
+  main = getReleases
